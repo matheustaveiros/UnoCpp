a23ec23191b43a2ee7c444d2f79c2e64738cfb5e
mtaveiros <mtaveiros@kokku.com.br>
2024-01-30 13:15:44 -0300

    Modified turn handler to protected in BaseAction

--- UnoCpp/Actions/BaseAction.h
+++ UnoCpp/Actions/BaseAction.h
@@ -5,6 +5,8 @@
 class TurnHandler;
 class BaseAction
 {
+protected:
+	std::shared_ptr<TurnHandler> _turnHandler;
 
 public:
 	explicit BaseAction(std::shared_ptr<TurnHandler> turnHandler);
@@ -12,7 +14,5 @@ public:
 	virtual void Execute() = 0;
 
 	virtual ~BaseAction() = default;
-
-	std::shared_ptr<TurnHandler> _turnHandler;
 };
 
